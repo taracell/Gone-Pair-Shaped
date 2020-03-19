f1a7175e57e394edf61c85ad1715b93da2954b9c
Ubuntu <ubuntu@ip-172-31-37-147.ec2.internal>
2020-03-19 17:38:04 +0000

    Merged

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from utils import help
 import discord
 
 with open('token.txt', 'r') as f:
-  token = [line.strip() for line in f]
+    token = [line.strip() for line in f]
 
 production = False
 try:
@@ -15,6 +15,7 @@ main_prefix = "$" if production else "£"
 cogs = [
     "jishaku",
     "cogs.cah",
+    "guildmanager.cog",
     "cogs.errors",
     "cogs.botlist"
 ]
@@ -38,6 +39,17 @@ bot.skips = []
 
 bot.main_prefix = main_prefix
 
+bot.owners = [
+    "PineappleFan#9955",
+    "Minion3665#6456",
+    "TheCodedProf#2583",
+]
+bot.helpers = {
+    "Waldigo#6969": "Programming help",
+    "nwunder#4018": "Programming help",
+    "Mine#4200": "Tester & legend",
+}
+
 
 @bot.event
 async def on_ready():
@@ -59,8 +71,24 @@ async def on_ready():
     )
 
 
+@bot.command()
+async def info(ctx):
+    embed = discord.Embed(
+        title='Cards Against Humanity - Commands',
+        description="> **STAFF**\n**Co-owners:**\n" + "\n".join("> " + user for user in bot.owners) +
+                    "\n**Helpers (Good people):**\n" + "\n".join(
+                    "> " + user + ": " + reason for user, reason in bot.helpers.items()) +
+                    "\n\n> **INVITE ME**\n[discordapp.com]"
+                    "(https://discordapp.com/oauth2/authorize?client_id=679361555732627476&scope=bot&permissions=130048"
+                    ")\n\n> **SERVER**\n[Cards Against Humanity Bot](https://discord.gg/bPaNnxe)",
+        color=discord.Color(0x8bc34a)
+    )
+    ctx.send(embed)
+
+
 file = open('token.txt', 'r')
 bot.tokens = [line.strip() for line in file]
 file.close()
 
 bot.run(bot.tokens[0])
+
