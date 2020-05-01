acb0b5f6bcc5e9de87503ab6cafe72882dda5252
Minion3665 <minion@clicksminuteper.net>
2020-05-01 23:21:04 +0100

    Not agreeing to the terms no longer makes us be obnoxious with not-found commands

--- cogs/cah/objects/game.py
+++ cogs/cah/objects/game.py
@@ -533,9 +533,9 @@ class Game:
                 picked_card_data.append(".".join(player_picks))
 
             this_round_data = (
-                self.question_data.keys()[self.question_data.values().index(question)],
+                self.question_data.keys()[list(self.question_data.values()).index(question)],
                 ",".join(picked_card_data),
-                self.answer_data.keys()[self.answer_data.values().index(winner.picked)]
+                self.answer_data.keys()[list(self.answer_data.values()).index(winner.picked)]
             )
 
             old_data = self.context.bot.AIDataStore.load_data()
